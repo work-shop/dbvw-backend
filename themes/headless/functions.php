eeb9993c12284d0a7b76970b6c7d25a682607313
Greg Nemes <mail@gregnemes.com>
2017-07-19 14:36:20 -0400

    added request to arguments for get_related_projects

--- themes/headless/functions.php
+++ themes/headless/functions.php
@@ -33,10 +33,10 @@ add_action( 'rest_api_init', function () {
 	//GET RID OF wp/v2!!!
 	register_rest_route( 'custom', '/relatedprojects', array(
 		'methods'   =>  'GET',
-		'callback'  =>  array($this,'get_relatedprojects'),
+		'callback'  =>  array($this,'get_related_projects'),
 		) );
 });
-function get_relatedprojects() {
+function get_related_projects( $request ) {
 	$project_category = $_GET['category'];
 	$current_project = $_GET['current'];
 
